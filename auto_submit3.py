45c2f3f326be6a289e7a7f8567f6f25355015b7f
trung501 <gotrungogle@gmail.com>
2022-11-05 11:39:31 +0700

    commit

---
+++ auto_submit3.py
@@ -0,0 +1,28 @@
+import subprocess
+from time import sleep
+
+    
+while True:
+    # try:
+    #     p = subprocess.Popen("python3 s.py", shell=True)
+    #     sleep(60)  
+    # except:
+    #     print("error get flag")
+    #     sleep(30)     
+    with open("web.txt","r") as f:
+        lines=f.readlines()
+        for line in lines:
+            line=line.replace('\n','').strip()
+            try:
+                if len(line)>0 :
+                    cmd_query=f"""curl -i -s -k -X PUT -H 'Host: 10.254.0.253:8080' -H 'Accept-Encoding: gzip, deflate' """ \
+                            f"""-H 'Accept: /' -H 'Connection: close' -H 'X-Team-Token: e5902a784c4ca765' """ \
+                            f"""-H 'Content-Length: 36' -H 'Content-Type: application/json' --data-binary """  \
+                            f"""'["{line}"]' 'http://10.254.0.253:8080/flags' """
+                    print(cmd_query)
+                    p = subprocess.Popen(cmd_query, shell=True)
+                    sleep(5)
+            except:
+                print("error curl")
+                sleep(30) 
+    sleep(300)
