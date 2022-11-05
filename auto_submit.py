2268e88ec1d154baedd8ef5cdc3b9308504e7c6d
trung501 <gotrungogle@gmail.com>
2022-11-05 10:14:02 +0700

    commit

---
+++ auto_submit.py
@@ -0,0 +1,19 @@
+import subprocess
+from time import sleep
+def get_flag():
+    return "PFE6FLXJMD1YDWCXIOL0V7GC9T0A9QT="
+
+    
+while True:   
+    with open("flag_pwn1.txt","r") as f:
+        lines=f.readlines()
+        for line in lines:
+            line=line.replace('\n','').strip()
+            if len(line)>0 :
+                cmd_query=f"""curl -i -s -k -X PUT -H 'Host: 10.254.0.253:8080' -H ''Accept-Encoding: gzip, deflate' """ \
+                        f"""-H 'Accept: /' -H 'Connection: close' -H 'X-Team-Token: e5902a784c4ca765' """ \
+                        f"""-H 'Content-Length: 36' -H 'Content-Type: application/json' --data-binary """  \
+                        f"""'["{line}"]' 'http://10.254.0.253:8080/flags' """
+                print(cmd_query)
+                p = subprocess.Popen(cmd_query, shell=True)
+    sleep(10)
