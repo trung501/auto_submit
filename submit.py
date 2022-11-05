790908c987cdd748116447176f9ab7e26a2c3211
trung501 <gotrungogle@gmail.com>
2022-11-05 13:10:51 +0700

    commit

--- submit.py
+++ submit.py
@@ -4,6 +4,7 @@ import argparse, os, random, sys, requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--flag', dest='flag', type=str)
+args = parser.parse_args()
 cmd_query=f"""curl -i -s -k -X PUT -H 'Host: 10.254.0.253:8080' -H 'Accept-Encoding: gzip, deflate' """ \
                             f"""-H 'Accept: /' -H 'Connection: close' -H 'X-Team-Token: e5902a784c4ca765' """ \
                             f"""-H 'Content-Length: 36' -H 'Content-Type: application/json' --data-binary """  \
