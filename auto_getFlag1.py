338c4b4ff4ed15767d5236d25e3a6c5efd98fa00
trung501 <gotrungogle@gmail.com>
2022-11-05 11:51:04 +0700

    commit

--- auto_getFlag1.py
+++ auto_getFlag1.py
@@ -1,6 +1,24 @@
+import requests
 import subprocess
 from time import sleep
 
+headers = {
+    'Host': '10.254.0.253:8080',
+    # 'Accept-Encoding': 'gzip, deflate',
+    'Accept': '/',
+    'Connection': 'close',
+    'X-Team-Token': 'e5902a784c4ca765',
+    # 'Content-Length': '36',
+    # Already added when you pass json=
+    # 'Content-Type': 'application/json',
+}
+
+json_data = [
+    'PMYI4B847OF06GHRUJVMP3EEXX2VXMK=',
+]
 while True:
-    p = subprocess.Popen("python3 pwn2.py", shell=True)
-    sleep(60)
+    try:    
+        response = requests.put('http://10.254.0.253:8080/flags', headers=headers, json=json_data, verify=False)
+        print(response.text)
+    except:
+        print('error')
